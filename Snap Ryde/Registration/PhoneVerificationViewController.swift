4ed9910b34f9c2cd7c4cbc2a06179d89ffb6d9f1
Muhammad Ahsan <mohammedahsan110@gmail.com>
2020-02-07 11:39:17 +0500

    Placing all code on github public repo

---
+++ Snap Ryde/Registration/PhoneVerificationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhoneVerificationViewController.swift
+//  Snap Ryde
+//
+//  Created by Tahir Raza on 05/12/2019.
+//  Copyright © 2019 Mohammed Ahsan. All rights reserved.
+//
+
+import UIKit
+
+class PhoneVerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Snap Ryde/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Snap Ryde
+//
+//  Created by Tahir Raza on 25/11/2019.
+//  Copyright © 2019 Mohammed Ahsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
